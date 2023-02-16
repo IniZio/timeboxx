c7a1410515971f9fab99c9154300547838d119ae
IniZio <newman.kcchow@gmail.com>
2023-02-17 00:37:40 +0800

    Setup unocss

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import '@unocss/reset/tailwind.css'
+
+import 'virtual:uno.css'
+import 'virtual:unocss-devtools'
+
 import App from './App'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import unocss from 'unocss/vite'
+import unocssPresetUno from '@unocss/preset-uno'
+import unocssAttributify from '@unocss/preset-attributify';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    unocss({
+      presets: [
+        unocssPresetUno(),
+        unocssAttributify(),
+      ]
+    }),
+  ],
 })
