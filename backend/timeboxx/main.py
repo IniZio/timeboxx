7c34d5514563ec378b7adb2733f871d2ae34b780
IniZio <newman.kcchow@gmail.com>
2023-02-18 10:41:09 +0800

    Sqlalchemy demo handler

--- backend/timeboxx/main.py
+++ backend/timeboxx/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 
+from timeboxx.pkg.db import db_session
+from timeboxx.pkg.db_models.user import User
+
 app = FastAPI()
 
 
 @app.get("/")
 async def main_route():
-    return {"message": "OK"}
+    with db_session() as db:
+        users = db.query(User).all()
+        user_ids = list(map(lambda u: u.id, users))
+
+        return {"message": user_ids}
