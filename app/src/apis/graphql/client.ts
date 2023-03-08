8da1081dd719282049b840e6e22c4163c1c0e668
IniZio <newman.kcchow@gmail.com>
2023-03-08 23:39:31 +0800

    Format web

--- app/src/apis/graphql/client.ts
+++ app/src/apis/graphql/client.ts
@@ -1,10 +1,9 @@
 // eslint-disable-next-line import/named
 import { createClient } from "urql";
 
-import { authClient } from '@/apis/auth/client';
-
+import { authClient } from "@/apis/auth/client";
 
 export const grahpqlClient = createClient({
   url: import.meta.env.VITE_APP_GRAPHQL_PUBLIC_ENDPOINT,
-  fetch: authClient.fetch
+  fetch: authClient.fetch,
 });
