477f07b743a8e3687dea083108ebcf0cab556e26
IniZio <newman.kcchow@gmail.com>
2023-03-15 00:54:26 +0800

    Only commit & close session at root resolver

--- backend/timeboxx/graphql/sqlalchemy_extension.py
+++ backend/timeboxx/graphql/sqlalchemy_extension.py
@@ -7,16 +7,30 @@ from strawberry.extensions import Extension
 # The sqlalchemy middlware doesn't work well with strawberry,
 # so we manually add the db session context here
 class SqlalchemyExtension(Extension):
+    def on_request_end(self):
+        super().on_request_end()
+
     async def resolve(self, _next, root, info, *args, **kwargs):
         session: AsyncSession = info.context.session
 
-        try:
+        # Only commit and close session at root resolver
+        if root:
             resolved = super().resolve(_next, root, info, *args, **kwargs)
-
             if isawaitable(resolved):
                 resolved = await resolved
 
-            if session.is_active:
+            return resolved
+
+        try:
+            resolved = super().resolve(_next, root, info, *args, **kwargs)
+            if isawaitable(resolved):
+                resolved = await resolved
+
+            if (
+                not root
+                and session.is_active
+                and (session.dirty or session.new or session.deleted)
+            ):
                 await session.commit()
 
             return resolved
