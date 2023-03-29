ba62690c8a5c35191ac3b98fb7827b622e3534e5
IniZio <newman.kcchow@gmail.com>
2023-03-29 09:19:48 +0800

    Yikes

--- backend/timeboxx/healthz.py
+++ backend/timeboxx/healthz.py
@@ -3,10 +3,9 @@ import traceback
 from dependency_injector.wiring import Provide, inject
 from fastapi import APIRouter, Depends
 from sqlalchemy import text
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Session
 
 from timeboxx.container import Container
-from timeboxx.pkg.config import MainSettings
 
 router = APIRouter()
 
@@ -14,17 +13,15 @@ router = APIRouter()
 @router.get("/healthz")
 @inject
 async def healthz_route(
-    settings: MainSettings = Depends(Provide[Container.settings]),
-    session: AsyncSession = Depends(Provide[Container.session]),
+    session: Session = Depends(Provide[Container.session]),
 ):
     db_healthy = True
     try:
-        print(await session.scalar(text("SELECT 1")))
+        print(session.scalar(text("SELECT 1")))
     except Exception as e:
         traceback.print_exception(e)
         db_healthy = False
 
     return {
         "db_healthy": db_healthy,
-        "key": settings.AUTHGEAR_ADMIN_API_KEY.replace("\\n", "\n"),
     }
